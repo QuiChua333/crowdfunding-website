b58bfa92382423e90c0aa04f617e0e91bf7f6dc2
Huynh Ngoc Qui <quichua333@gmail.com>
2023-10-10 16:48:46 +0700

    fix header

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,14 @@ import Following from '~/pages/Following'
 import Upload from '~/pages/Upload'
 import { HeaderOnlyLayout } from '~/components/Layout'
 import { HomeLayout } from '~/components/Layout'
+import Explore from '~/pages/user/Explore'
 
 // Public routes
 const publicRoutes = [
     { path: '/', component: Home, layout: HomeLayout},
     { path: '/following', component: Following},
-    { path: '/upload', component: Upload}
+    { path: '/upload', component: Upload},
+    { path: '/explore/:type', component : Explore, layout: HeaderOnlyLayout }
 ]
 
 const privateRoutes = [
